6d0d9f789b0e8ecbbc6c59dae9c28c8f5ee541b9
MJ <themjtechnique@gmail.com>
2025-03-28 01:57:08 -0500

    Nuevo punto 5

--- task5.js
+++ task5.js
@@ -1,2 +1,29 @@
 // Red social, filtro por edad
 // No sé ni por dónde empezar
+
+const prompt = require("prompt-sync")()
+
+let listaUsuarios = [
+    {nombre: "Andres", edad: 23},
+    {nombre: "Majo", edad: 14},
+    {nombre: "Felipe", edad: 20},
+    {nombre: "Omar", edad: 50},
+    {nombre: "Daniel", edad: 18},
+    {nombre: "Mapu", edad: 24}
+];
+
+function filtrarUsuarios(usuarios, edadMinima) {
+    let lista2 = [];
+
+    for (let i = 0; i < usuarios.length; i++){
+        if (usuarios[i].edad >= edadMinima){
+          lista2.push(usuarios[i]);
+        }
+    }
+    return lista2;
+ }
+
+ let edadMinima = Number(prompt("Ingrese la edad mínima: "))
+ let nuevoFiltro = filtrarUsuarios(listaUsuarios, edadMinima);
+
+ console.log("Usuarios que cumplen con la edad mínima: ", (nuevoFiltro))
