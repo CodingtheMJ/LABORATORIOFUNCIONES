cd554754ffc7a11e3d66b11a7db47f1a63d8564b
MJ <themjtechnique@gmail.com>
2025-03-28 04:37:33 -0500

    Nuevos comentarios en task 1 y 2

--- task1.js
+++ task1.js
@@ -1,17 +1,17 @@
 // Calculadora de propinaaas
 
-const prompt = require("prompt-sync")();               //Constante para el prompt de total y propinas
+const prompt = require("prompt-sync")();                                                //Constante para el prompt de total y propinas
 
 let total = Number(prompt("Introduzca monto total de la factura: ")) 
 let porcentaje = Number(prompt("Introduzca cuánto es el porcentaje de la propina: "))
 
 
-                                                       // Función para que agarre tanto el total como el porcentaje.
+                                                                                        // Función para que agarre tanto el total como el porcentaje.
 function calcularPropina(total, porcentaje) {
-    let propina = (total * porcentaje) / 100;           //defines propina basándote en la matemática de un porcentaje
-    let resultado = total + propina;                    //Defines un resultado total para el valor total junto con la propina
-    return resultado;                                   //Haces returno solo para poder utilizar resultado
+    let propina = (total * porcentaje) / 100;                                            //Defines propina basándote en la matemática de un porcentaje
+    let resultado = total + propina;                                                     //Defines un resultado total para el valor total junto con la propina
+    return resultado;                                                                    //Haces returno solo para poder utilizar resultado
 }
-    let totalPagar = calcularPropina(total, porcentaje); //defines cuál sería tu total a pagar con base en la anterior función
-    console.log("Total sin propina: $", {total}, "Total con propina: $", {totalPagar})
+    let totalPagar = calcularPropina(total, porcentaje);                                //Defines cuál sería tu total a pagar con base en la anterior función
+    console.log("Total sin propina: $", {total}, "Total con propina: $", {totalPagar})  //Imprimes :3
 
--- task2.js
+++ task2.js
@@ -1,31 +1,31 @@
-const prompt = require("prompt-sync")();
+const prompt = require("prompt-sync")();                                                    //Traigo la constante para que funcionen los prompts
 
-function validar(pwsrd) {
-    for (let i = 0; i < pwsrd.length; i++) {
-        let caracter = pwsrd[i];
+function validar(pwsrd) {                                                                   //Hago función para validar la contraseña
+    for (let i = 0; i < pwsrd.length; i++) {                                                //Defino mi contador para cada caracter de la contraseña
+        let caracter = pwsrd[i];                                                            //Defino variable para que cada vez que la llame, se recorran los carácteres de contraseña
 
-        if (caracter === caracter.toUpperCase() && caracter >= "A" && caracter <= "Z") {
+        if (caracter === caracter.toUpperCase() && caracter >= "A" && caracter <= "Z") {    //si la contraseña tiene mayúscula dentro del diccionario de A a la Z, entonces se cumple la función
             tieneMayuscula = true;
         }
 
-        if (caracter >= "0" && caracter <= "9") {
+        if (caracter >= "0" && caracter <= "9") {                                           //Si la contraseña tiene un número del 0 al 9, cumple la función
             tieneNumero = true;
         }
     }
 
-    let tieneLongitud = pwsrd.length >= 8;
+    let tieneLongitud = pwsrd.length >= 8;                                                  //Definir variable para saber si la contraseña es del largo adecuado
 
-    return tieneMayuscula && tieneNumero && tieneLongitud;
+    return tieneMayuscula && tieneNumero && tieneLongitud;                                  //Volver a traer todos los parámetros
 }
 
-function main() {
-    let pwsrd = prompt("Ingresa tu contraseña: ");
+function main() {                                                                           //función nueva main
+    let pwsrd = prompt("Ingresa tu contraseña: ");                                          //definir el prompt
     
-    if (validar(pwsrd)) {
-        console.log("¡Tu contraseña es correcta!");
+    if (validar(pwsrd)) {                                                                   //traer la función y aplicarla en pswrd como prompt
+        console.log("¡Tu contraseña es correcta!");                                         //Imprimir si es válida
     } else {
-        console.log("Contraseña incorrecta. Inténtalo de nuevo.");
+        console.log("Contraseña incorrecta. Inténtalo de nuevo.");                          //Imprimir si no es válido
     }
 }
 
-main();
+main();                                                                                     //Traer función 2 (main) para que rompa bucle y se imprima
